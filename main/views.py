aae6da797687e88b9368285f0c2b7be65ea9222f
Batyrzhan <batyrzhantogaev2002@icloud.com>
2025-06-27 21:46:51 +0500

    Creation of logic for sending and saving to the database, as well as standard Django views for registration and authentication.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Message
+from django.db.models import Q
+from .models import Message
 
-# Create your views here.
+
+@login_required
+def chat_view(request, username):
+    target_user = get_object_or_404(User, username=username)
+
+    if request.method == 'POST':
+        text = request.POST.get('message')
+        if text:
+            Message.objects.create(
+                sender=request.user,
+                receiver=target_user,
+                text=text
+            )
+              # чтобы избежать повторной отправки при обновлении
+
+    messages = Message.objects.filter(
+        Q(sender=request.user, receiver=target_user) |
+        Q(sender=target_user, receiver=request.user)
+    ).order_by('timestamp')
+
+    return render(request, 'main/chat.html', {
+        'target_user': target_user,
+        'messages': messages
+    })
