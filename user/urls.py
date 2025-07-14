4f6def2924f46ebfa9d1b3b61e1bb6f6190ca04a
Batyrzhan <batyrzhantogaev2002@icloud.com>
2025-07-14 14:05:59 +0500

    Completed project. Fully working user registration and authentication logic, message sending between two users via WebSockets.

--- user/urls.py
+++ user/urls.py
@@ -5,9 +5,8 @@ from .views import register
 
 urlpatterns = [
     path('login/', LoginView.as_view(
-        redirect_authenticated_user=True
-        ),
-        name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+        redirect_authenticated_user=True), name='login'),
+    path('logout/', LogoutView.as_view(
+            next_page='login'), name='logout'),
     path('register/', register, name='register')
 ]
